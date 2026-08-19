9df562d27d4b97f0429567e3b321982b15e7696f
AdityaPandey98-Dev <adityapandey98.dev@gmail.com>
2026-08-19 19:46:49 +0530

    Updated

---
+++ day-03/backend/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    int[] arr1 = new int[5];
+    arr1[0] = 5;
+    arr1[0] = 10;
+    arr1[0] = 15;
+    arr1[0] = 20;
+    arr1[0] = 25;
+    System.out.println(arr1) // will print addrerss
+    System.out.println(Arrays.toString(arr1)); // will print arrays
+}
